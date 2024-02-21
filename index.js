5c0fc0025b1eace1db96a8419737057825e3ac98
TmsLzn <tomasito.lozano@gmail.com>
2024-02-21 11:52:30 +0100

    h

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let app = express();
 //Puerto en el que se despliega
 app.listen(PORT);
 app.get("/cool", (req,res)=>{
-res.send("<html><body><h1> ${cool} </h1></body></html>")
+res.send(`<html><body><h1>${cool}</h1></body></html>`)
 });
 //console.log(cool());
 console.log("hola caracola");
