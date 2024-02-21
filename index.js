57e28b97a8a8a55fa920838435a097ec28246d79
suarovski <alvsuacar@alum.us.es>
2024-02-21 11:51:41 +0100

    Cambios index.js

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 let cool = require("cool-ascii-faces");
 let express = require("express");
-const PORT = 8080;
+
 let app = express();
+const PORT = (process.env.PORT || 8080);
+
+
 //Puerto en el que se despliega
 app.listen(PORT);
 app.get("/cool", (req,res)=>{
-res.send("<html><body><h1> ${cool} </h1></body></html>")
+    res.send("<html><body><h1> ${cool} </h1></body></html>")
 });
 //console.log(cool());
-console.log("hola caracola");
+console.log(`Server listening on port ${PORT}`);
