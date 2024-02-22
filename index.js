c832f202dc6e02854a9ecf0684d7a778a79e1a9d
TmsLzn <tomasito.lozano@gmail.com>
2024-02-22 16:40:50 +0100

    Eliminar node_module

--- index.js
+++ index.js
@@ -6,10 +6,17 @@ const PORT = (process.env.PORT || 8080);
 
 app.use("/static/",express.static("./public/index.html"))
 
+//Funciones index-XXX.js
+app.use("/samples/TLR",express.static("./index-TLR.js"))
+app.use("/samples/MRF",express.static("./index-MRF.js"))
+app.use("/samples/ASC",express.static("./index-ASC.js"))
+app.use("/samples/ASB",express.static("./index-ASB.js"))
+
 //Puerto en el que se despliega
 app.listen(PORT);
 app.get("/cool", (req,res)=>{
     res.send(`<html><body><h1>${cool()}</h1></body></html>`)
 });
+
 //console.log(cool());
 console.log(`Server listening on port ${PORT}`);
