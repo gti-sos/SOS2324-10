f18f7e0140d25be5d02ffabb9245c9e18ad35ca9
TmsLzn <tomasito.lozano@gmail.com>
2024-02-22 18:06:00 +0100

    tomas

--- index.js
+++ index.js
@@ -3,20 +3,48 @@ let express = require("express");
 
 let app = express();
 const PORT = (process.env.PORT || 8080);
-
-app.use("/static/",express.static("./public/index.html"))
-
-//Funciones index-XXX.js
-app.use("/samples/TLR",express.static("./index-TLR.js"))
-app.use("/samples/MRF",express.static("./index-MRF.js"))
-app.use("/samples/ASC",express.static("./index-ASC.js"))
-app.use("/samples/ASB",express.static("./index-ASB.js"))
-
-//Puerto en el que se despliega
 app.listen(PORT);
+
+//Establecemos subdirectorios de la web
+const path = require('path');
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use("/",express.static("./public"));
+
 app.get("/cool", (req,res)=>{
     res.send(`<html><body><h1>${cool()}</h1></body></html>`)
 });
 
-//console.log(cool());
 console.log(`Server listening on port ${PORT}`);
+
+//Función index-TLR.js
+const datos_TLR = require('./index-TLR');
+function calcularMediasMuertesPorPais(datos_TLR) {
+    const muertesPorPais = {};
+    datos_TLR.forEach((dato) => {
+      const pais = dato.geo;
+      const muertes = dato.cars_deaths;
+      if (!muertesPorPais[pais]) {
+        muertesPorPais[pais] = { totalMuertes: 0, conteo: 0 };
+      }
+      muertesPorPais[pais].totalMuertes += muertes;
+      muertesPorPais[pais].conteo++;
+    });
+  
+    const mediaMuertesPorPais = {};
+    for (const pais in muertesPorPais) {
+      const totalMuertes = muertesPorPais[pais].totalMuertes;
+      const conteo = muertesPorPais[pais].conteo;
+      mediaMuertesPorPais[pais] = totalMuertes / conteo;
+    }
+  
+    return mediaMuertesPorPais;
+}
+
+app.get("/samples/TLR", (req,res)=>{
+    const mediaMuertesPorPais = calcularMediasMuertesPorPais(datos_TLR); 
+    const mediaMuertesJSON = JSON.stringify(mediaMuertesPorPais);
+    res.send(`<html> <body> <h1> ${mediaMuertesJSON}</h1> </body> </html>`)
+});
