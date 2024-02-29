a4b3be9f0c8727ccbab3bb2a274bb3b63cefbd78
suarovski <alvsuacar@alum.us.es>
2024-02-29 18:07:28 +0100

    Alvaro

--- index-ASC.js
+++ index-ASC.js
@@ -1,53 +1,323 @@
-let csvData = `dataflow,last_update,freq,vehicle,unit,geo,time_period,obs_value,flights_passangers,cars_deaths
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,BG,2020,2866763,null,75.4
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,BG,2021,2830464,7.7,72.3
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,CZ,2013,4729185,null,0.0
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,CZ,2014,4833386,2.3,100.7
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,LT,2014,1205668,null,3.5
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,LT,2015,1244063,2.0,100.0
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,ES,2016,22876830,3.0,100.6
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,ES,2017,23500401,3.0,94.4
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,PL,2015,18011775,null,4.4
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,PL,2016,18718408,null,3.0
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y10-20,IT,2017,21318885,1.9,105.3
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y10-20,IT,2018,21965599,1.1,102.0
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y_LT2,SE,2018,936840,0.8,83.5
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y_LT2,SE,2019,882084,1.0,82.2
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y_LT2,HU,2016,204007,2.2,103.1
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y_LT2,HU,2017,244719,4.3,103.5
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,EU27_2020,2018,240415203,1.9,96.6
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,EU27_2020,2019,244951204,1.6,95.4
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,EU27_2020,2020,247754214,-5.7,79.2
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y2-5,AT,2018,874539,2.4,90.7
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y2-5,AT,2019,900900,1.5,87.5
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,BG,2013,2910235,-0.5,94.4
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,BG,2014,3013863,0.9,99.1
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y5-10,CH,2015,1315144,1.6,100.0
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y5-10,CH,2016,1372084,2.1,99.8
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y5-10,CH,2017,1428311,1.4,99.6
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,DE,2019,47715977,1.1,91.1
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,TOTAL,DE,2020,48248584,-3.8,79.9
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y_GT20,UK,2016,572233,1.7,99.4
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y_GT20,UK,2017,617953,1.7,103.6
-ESTAT:ROAD_EQS_CARAGE(1.0),19/12/23 23:00:00,A,NR,Y_GT20,UK,2018,692318,1.3,103.8
-`;
-
-
-
-let lines = csvData
-.split('\n');
-let columnas = lines[0].split(',');
-let csv = [];
-
-
-for (let i = 1; i < lines.length; i++) {
-    let elementos = lines[i].split(',');
-    let mapa = new Map();
-    for (let j = 0; j < columnas.length; j++) {
-        mapa.set(columnas[j], elementos[j]);
+const data = [
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'BG',
+        time_period: 2020,
+        obs_value: 2866763,
+        gdp: -4,
+        volgdp: 75.4
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'BG',
+        time_period: 2021,
+        obs_value: 2830464,
+        gdp: 7.7,
+        volgdp: 72.3
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'CZ',
+        time_period: 2013,
+        obs_value: 4729185,
+        gdp: 0.0,
+        volgdp: 99.6
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'CZ',
+        time_period: 2014,
+        obs_value: 4833386,
+        gdp: 2.3,
+        volgdp: 100.7
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'LT',
+        time_period: 2014,
+        obs_value: 1205668,
+        gdp: 3.5,
+        volgdp: 101.1
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'LT',
+        time_period: 2015,
+        obs_value: 1244063,
+        gdp: 2.0,
+        volgdp: 100.0
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'ES',
+        time_period: 2016,
+        obs_value: 22876830,
+        gdp: 3.0,
+        volgdp: 100.6
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'ES',
+        time_period: 2017,
+        obs_value: 23500401,
+        gdp: 3.0,
+        volgdp: 94.4
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'PL',
+        time_period: 2015,
+        obs_value: 18011775,
+        gdp: 4.4,
+        volgdp: 100.0
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'PL',
+        time_period: 2016,
+        obs_value: 18718408,
+        gdp: 3.0,
+        volgdp: 101.8
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y10-20',
+        geo: 'IT',
+        time_period: 2017,
+        obs_value: 21318885,
+        gdp: 1.9,
+        volgdp: 105.3
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y10-20',
+        geo: 'IT',
+        time_period: 2018,
+        obs_value: 21965599,
+        gdp: 1.1,
+        volgdp: 102.0
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y_LT2',
+        geo: 'SE',
+        time_period: 2018,
+        obs_value: 936840,
+        gdp: 0.8,
+        volgdp: 83.5
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y_LT2',
+        geo: 'SE',
+        time_period: 2019,
+        obs_value: 882084,
+        gdp: 1.0,
+        volgdp: 82.2
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y_LT2',
+        geo: 'HU',
+        time_period: 2016,
+       
+    
+    obs_value: 204007,
+        gdp: 2.2,
+        volgdp: 103.1
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y_LT2',
+        geo: 'HU',
+        time_period: 2017,
+        obs_value: 244719,
+        gdp: 4.3,
+        volgdp: 103.5
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'EU27_2020',
+        time_period: 2018,
+        obs_value: 240415203,
+        gdp: 1.9,
+        volgdp: 96.6
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'EU27_2020',
+        time_period: 2019,
+        obs_value: 244951204,
+        gdp: 1.6,
+        volgdp: 95.4
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'EU27_2020',
+        time_period: 2020,
+        obs_value: 247754214,
+        gdp: -5.7,
+        volgdp: 79.2
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y2-5',
+        geo: 'AT',
+        time_period: 2018,
+        obs_value: 874539,
+        gdp: 2.4,
+        volgdp: 90.7
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y2-5',
+        geo: 'AT',
+        time_period: 2019,
+        obs_value: 900900,
+        gdp: 1.5,
+        volgdp: 87.5
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'BG',
+        time_period: 2013,
+        obs_value: 2910235,
+        gdp: -0.5,
+        volgdp: 94.4
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'BG',
+        time_period: 2014,
+        obs_value: 3013863,
+        gdp: 0.9,
+        volgdp: 99.1
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y5-10',
+        geo: 'CH',
+        time_period: 2015,
+        obs_value: 1315144,
+        gdp: 1.6,
+        volgdp: 100.0
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y5-10',
+        geo: 'CH',
+        time_period: 2016,
+        obs_value: 1372084,
+        gdp: 2.1,
+        volgdp: 99.8
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y5-10',
+        geo: 'CH',
+        time_period: 2017,
+        obs_value: 1428311,
+        gdp: 1.4,
+        volgdp: 99.6
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'DE',
+        time_period: 2019,
+        obs_value: 47715977,
+        gdp: 1.1,
+        volgdp: 91.1
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'TOTAL',
+        geo: 'DE',
+        time_period: 2020,
+        obs_value: 48248584,
+        gdp: -3.8,
+        volgdp: 79.9
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y_GT20',
+        geo: 'UK',
+        time_period: 2016,
+        obs_value: 572233,
+        gdp: 1.7,
+        volgdp: 99.4
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y_GT20',
+        geo: 'UK',
+        time_period: 2017,
+        obs_value: 617953,
+        gdp: 1.7,
+        volgdp: 103.6
+    },
+    {
+        frequency: 'A',
+        unit: 'NR',
+        age: 'Y_GT20',
+        geo: 'UK',
+        time_period: 2018,
+        obs_value: 692318,
+        gdp: 1.3,
+        volgdp: 103.8
     }
-    csv.push(mapa);
-}
+
+]
+
+const csv = data.map((entry, index) => {
+    return { id: index + 1, ...rest };
+});
+
 module.exports = csv;
 
 function calcularMediaObsValuePorPais(csv) {
