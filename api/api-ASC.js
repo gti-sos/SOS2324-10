98e5171e9e8dc53304416c1330a436eef259c7b7
suarovski <alvsuacar@alum.us.es>
2024-02-29 20:01:21 +0100

    apiASC

--- api/api-ASC.js
+++ api/api-ASC.js
@@ -10,4 +10,21 @@ module.exports = (app) => {
     app.get(API_BASE + "/datosasc", (req, res) => {
         res.send(JSON.stringify(csv));
     });
+
+    app.get(API_BASE + "/datosasc/loadInitialData", (req, res) => {
+        if(csv == null){
+            res.send(JSON.stringify(csv));
+        }
+    });
+
+    app.post(API_BASE + "/datosasc", (req, res) => {
+        let growth = req.body;
+        csv.push(growth);
+        res.sendStatus(201, "Created");
+    });
+    
+    app.delete(API_BASE + "/datosasc", (req, res) => {
+        csv.splice(0, csv.length); 
+        res.sendStatus(200,"Deleted all -> Datos ASC");
+    }); 
 };
