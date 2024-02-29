60cabcaa3a95af7143e93ae55a6b5e7119a0fe85
suarovski <alvsuacar@alum.us.es>
2024-02-29 19:43:19 +0100

    apiASC

--- api/api-ASC.js
+++ api/api-ASC.js
@@ -7,7 +7,7 @@ const csv = require('../index-ASC');
 
 
 module.exports = (app) => {
-    app.get(API_BASE + "/datosASC", (req, res) => {
+    app.get(API_BASE + "/datosasc", (req, res) => {
         res.send(JSON.stringify(csv));
     });
 };
