fbb9f25e61ff7e4c6706a09a7374ae9ee96800a5
antsuebae <antsuebae@alum.us.es>
2024-03-03 20:15:36 +0100

    cambios

--- api/api-ASB.js
+++ api/api-ASB.js
@@ -7,6 +7,7 @@ const datos = require('../index-ASB');
 
 
 module.exports = (app) => {
+    //GET
     app.get(API_BASE + "/datosasb", (req, res) => {
         res.send(JSON.stringify(datos));
     });
@@ -15,18 +16,21 @@ module.exports = (app) => {
             res.send(JSON.stringify(datos));
         }
     });
-    // app.post(API_BASE + "/datosasb", (req, res) => {
-    //     if (datos.some(entry => entry.id === req.body.id)) {
-    //         res.status(409).send("Conflict: Resource already exists");
-    //     } else {
-    //         // El recurso no existe, agregarlo a csv
-    //         datos.push(req.body);
-    //         res.status(201).send("Created");
-    //     }
-    // });
-    
-    app.post(API_BASE + "/datosasc", (req, res) => {
-        let datos = req.body;
+    app.get(API_BASE + "datosasc/:geo", (req, res) => {
+        const pais = req.params.geo;
+        const restr = datos.filter(n => n.geo === pais);
+
+        if (restr.length > 0) {
+            res.send(JSON.stringify(restr));
+            res.sendStatus(200, "OK");
+        } else {
+            res.sendStatus(404, "NOT FOUND");
+        }
+
+    });
+    //POST
+    app.post(API_BASE + "/datosasb", (req, res) => {
+        let data = req.body;
     
         // Verificar si el body es un JSON válido y tiene la estructura esperada
         const structure = {
@@ -44,20 +48,46 @@ module.exports = (app) => {
             'road_deaths_per_million_inhabitants': 'number'
         };
     
-        const actualKeys = Object.keys(datos);
+        const actualKeys = Object.keys(data);
         const expectedKeys = Object.keys(structure);
-        const isValidJson = expectedKeys.every(key => actualKeys.includes(key) && typeof datos[key] === structure[key]);
+        const isValidJson = expectedKeys.every(key => actualKeys.includes(key) && typeof data[key] === structure[key]);
     
         if (!isValidJson || actualKeys.length !== expectedKeys.length) {
             // El JSON no tiene la estructura esperada
             res.status(400).send("Bad Request: JSON has invalid structure");
-        } else if (datos.some(entry => entry.id === datos.id)) {
+        } else if (datos.some(entry => entry.id === data.id)) {
             // El recurso ya existe, devolver error 409
             res.status(409).send("Conflict: Resource already exists");
         } else {
             // El recurso no existe, agregarlo a csv
-            data.push(datos);
+            datos.push(datos);
             res.status(201).send("Created");
         }
     });
+    //PUT
+    app.put(API_BASE + "/datosasb/:id", (req, res) => {
+        const id = req.params.id;
+        const newData = req.body;
+
+        // Verificar si el dato tiene el mismo id del recurso en la URL
+        if (id !== newData.id) {
+            res.status(400).send("Bad Request: ID mismatch");
+        } else {
+            // Actualizar el recurso si existe, de lo contrario, devolver error 404
+            const index = datos.findIndex(entry => entry.id === id);
+            if (index !== -1) {
+                datos[index] = newData;
+                res.status(200).send("Updated");
+            } else {
+                res.status(404).send("Not Found");
+            }
+        }
+    });
+    //DELETE
+    app.delete(API_BASE + "/b", (req, res) => {
+        datos.splice(0, datos.length);
+        res.sendStatus(200, "Deleted all -> Datos ASB");
+    });
+
+
 };  
