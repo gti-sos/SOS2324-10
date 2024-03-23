4d2983f1276902bf2d4d1b8088b5a52da6aed7e9
TmsLzn <tomasito.lozano@gmail.com>
2024-03-23 14:25:52 +0100

    tomas

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,2 +1,72 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Proyecto SOS2324-10</title>
+    <style>
+        body {
+            font-family: Arial, sans-serif;
+            line-height: 1.6;
+            margin: 0;
+            padding: 0;
+            background-color: #f3f7ff; /* Azul claro */
+            color: #333;
+        }
+
+        .container {
+            width: 80%;
+            margin: 50px auto;
+            background-color: #fff; /* Blanco */
+            border: 1px solid #a4caef; /* Azul claro */
+            border-radius: 5px;
+            box-shadow: 0 2px 5px rgba(0,0,0,0.1);
+            padding: 20px;
+        }
+
+        h1, h2, h3, h4, h5, h6 {
+            color: #0366d6; /* Azul */
+        }
+
+        pre {
+            white-space: pre-wrap;
+            word-wrap: break-word;
+            background-color: #f0f7ff; /* Azul claro */
+            border-radius: 5px;
+            padding: 10px;
+        }
+
+        a {
+            color: #0366d6; /* Azul */
+            text-decoration: none;
+        }
+
+        a:hover {
+            text-decoration: underline;
+        }
+    </style>
+</head>
+<body>
+    <div class="container">
+        <h1>Proyecto SOS2324-10</h1>
+        <p>SOS2324-10</p>
+        <p>Equipo</p>
+        <ul>
+            <li>Álvaro Suárez Carbonell</li>
+            <li>Antonio Suero Baeza</li>
+            <li>Miguel Robledo Fernández</li>
+            <li>Tomás Lozano Romero</li>
+        </ul>
+        <p>Descripción del proyecto: Nuestro proyecto se basa en analizar cómo afecta el PIB a los vehículos en Europa.</p>
+        <p>Repositorio: <a href="https://github.com/gti-sos/SOS2324-10">gti-sos/SOS2324-10</a></p>
+        <p>URL: <a href="https://sos2324-10.appspot.com/"></a></p>
+        <h2>API:</h2>
+        <ul>
+            <li>API Álvaro: <a href="https://sos2324-10.appspot.com/api/v1/tourisms-per-age">API-ASC</a> (developed by Álvaro Súarez Carbonell)</li>
+            <li>API Tomás: <a href="https://sos2324-10.appspot.com/api/v1/vehicles-stock/docs"> API-TLR</a> (developed by Tomás Lozano)</li>
+            <li>API Miguel: <a href="http://sos2324-10.appspot.com/api/v1/gdp-growth-rates/docs"> API-MRF </a> (developed by Miguel Robledo Fernández)</li>
+            <li>API Antonio: <a href="http://sos2324-10.appspot.com/api/v1/cars-by-motor"> API-ASB</a> (developed by Antonio Suero Baeza)</li>
+        </ul>
+    </div>
+</body>
+</html>
