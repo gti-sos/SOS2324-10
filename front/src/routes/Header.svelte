7311c85801793de60ef0d00ccae558272982cd02
suarovski <alvsuacar@alum.us.es>
2024-03-28 11:35:05 +0100

    Cambios

--- front/src/routes/Header.svelte
+++ front/src/routes/Header.svelte
@@ -3,7 +3,7 @@
     <a href="/vehicles-stock"> TLR_Front</a>
     <a href="/gdp-growth-rates"> MRF_Front</a>
     <a href="/cars-by-motor"> ASB_Front</a>
-    <a href="/tourism-per-age"> ASB_Front</a>
+    <a href="/tourism-per-age"> ASC_Front</a>
 </nav>
 
 <style>
