daf024b715eaf1dca94e8d770ce698adfe687410
suarovski <alvsuacar@alum.us.es>
2024-04-07 21:37:52 +0200

    FinASC

--- tests/e2e/tourisms-per-age/test-ASC.spec.cjs
+++ tests/e2e/tourisms-per-age/test-ASC.spec.cjs
@@ -1,14 +1,52 @@
-// @ts-check
-const { test, expect } = require('@playwright/test');
+const { test, expect, beforeAll, afterAll, beforeEach, afterEach } = require('@playwright/test');
 
-test('Carga página principal', async ({ page }) => {
-  await page.goto('http://localhost:8080/tourisms-per-age');
-  await expect(page).toHaveTitle('tourisms-per-age');
-  await expect(page).toHaveURL('http://localhost:8080/tourisms-per-age');
+let page;
+
+beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
 });
 
-test('has title Proyecto SOS2324-10', async ({ page }) => {
+afterAll(async () => {
+    await page.close();
+});
+
+
+test('Carga página principal', async () => {
+    await page.goto('http://localhost:8080/tourisms-per-age');
+    await expect(page).toHaveTitle('tourisms-per-age');
+    await expect(page).toHaveURL('http://localhost:8080/tourisms-per-age');
+});
+
+test('has title Proyecto SOS2324-10', async () => {
     test.setTimeout(60000);
     await page.goto('http://localhost:8080');
     await expect(page).toHaveTitle(/Proyecto SOS2324-10/);
-  });
+});
+
+test('more than 5 rows are loaded in the table', async () => {
+    await page.goto('http://localhost:8080/tourisms-per-age');
+    // Espera a que la tabla se cargue y obtiene la cantidad de filas
+    await page.waitForSelector('table');
+    const rowCount = await page.$$eval('table tbody tr', rows => rows.length);
+
+    // Verifica que la cantidad de filas sea mayor que 5
+    expect(rowCount).toBeGreaterThan(5);
+});
+
+test('delete a row from the table', async () => {
+    await page.goto('http://localhost:8080/tourisms-per-age');
+    // Obtiene el último elemento de la tabla antes de la eliminación
+    const lastElementBeforeDelete = await page.$eval('tbody tr:last-child', row => row.textContent);
+
+    // Elimina un elemento de la tabla
+    await page.click('button:has-text("Eliminar")');
+
+    // Espera un momento para que se procese la eliminación y se actualice la tabla
+    await page.waitForTimeout(1000);
+
+    // Obtiene el último elemento de la tabla después de la eliminación
+    const lastElementAfterDelete = await page.$eval('tbody tr:last-child', row => row.textContent);
+
+    // Verifica que el último elemento de la tabla después de la eliminación sea diferente al último elemento antes de la eliminación
+    expect(lastElementAfterDelete).not.toEqual(lastElementBeforeDelete);
+});
