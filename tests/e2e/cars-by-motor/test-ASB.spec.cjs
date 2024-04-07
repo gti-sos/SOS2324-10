b75963048fc3794b9d36f945c78687d8fd507ff0
antsuebae <antsuebae@alum.us.es>
2024-04-07 19:01:42 +0200

    m

--- tests/e2e/cars-by-motor/test-ASB.spec.cjs
+++ tests/e2e/cars-by-motor/test-ASB.spec.cjs
@@ -1,9 +1,38 @@
 /** 
 const { test, expect } = require('@playwright/test');
 
-test('Carga de cars-by-motor', async ({ page }) => {
-  await page.goto('https://sos2324-10.appspot.com/cars-by-motor');
-  await expect(page).toHaveTitle('cars-by-motor');
-  await expect(page).toHaveURL('https://sos2324-10.appspot.com/cars-by-motor');
+test('Titulo', async ({ page }) => {
+    await page.goto('http://localhost:8080/cars-by-motor');
+    await expect(page).toHaveTitle('cars-by-motor');
+    await expect(page).toHaveURL('http://localhost:8080/cars-by-motor');
 });
-*/
+
+test('delete car', async () => {
+    test.setTimeout(60000);
+    await page.goto('http://localhost:8080/api/v2/cars-by-motor/loadInitialData');
+    await page.waitForTimeout(1000);
+    await page.goto('http://localhost:8080/cars-by-motor');
+    await page.waitForLoadState('load');
+    await page.waitForTimeout(1000);
+    let carTitleBeforeDelete = await page.textContent('.tarjeta:first-child .card-title');
+    await page.click('#deletecarButton');
+    await page.waitForTimeout(1000);
+    let carTitleAfterDelete = await page.textContent('.tarjeta:first-child .card-title');
+    expect(carTitleAfterDelete).not.toEqual(carTitleBeforeDelete);
+});
+test('delete cars', async () => {
+    test.setTimeout(90000);
+    await page.goto('http://localhost:8080/api/v2/cars-by-motor/loadInitialData');
+    await page.waitForTimeout(1000);
+    await page.goto('http://localhost:8080/cars-by-motor');
+    await page.waitForLoadState('load');
+    await page.waitForTimeout(1000);
+    let cardElements = await page.locator('.tarjetas-datos').count();
+    expect(cardElements).toBeGreaterThan(5);
+    await page.click('#deleteAllButton');
+    await page.waitForTimeout(500);
+    await page.click('#deleteAllButtonConfirm');
+    await page.waitForTimeout(8080);
+    let updatedCardElements = await page.locator('.tarjetas-datos').count();
+    expect(updatedCardElements).toBe(0);
+});
