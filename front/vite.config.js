d15b8b3a13f275ea5e7c0c522ec819e09c0b64c2
TmsLzn <tomasito.lozano@gmail.com>
2024-05-16 20:25:56 +0200

    H

--- front/vite.config.js
+++ front/vite.config.js
@@ -1,11 +1,13 @@
-import { sveltekit } from '@sveltejs/kit/vite';
+// vite.config.js
+
 import { defineConfig } from 'vite';
+import sveltekit from '@sveltejs/adapter-static';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	build: {
-		rollupOptions: {
-			external: ['echarts']
-		}
-	}
+  plugins: [sveltekit()],
+  build: {
+    rollupOptions: {
+      external: ['echarts', 'd3']
+    }
+  }
 });
